48e0fe0bfa6c72caa707d248fe328b71052f210d
William Wong <compulim@hotmail.com>
2017-03-07 21:56:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# react-sanitized-html
+
+A React component that will sanitize user-inputted HTML code, using the popular [`sanitize-html`](https://npmjs.com/package/sanitize-html) package.
+
+# Install
+
+This React component requires both [`react`](https://npmjs.com/package/react) and [`sanitize-html`](https://npmjs.com/package/sanitize-html) to be installed to work. We marked both as peer dependency so you could use the version of React as it fit.
+
+To install this package, `npm install react-sanitized-html sanitize-html --save`
+
+# Example usage
+
+```jsx
+import SanitizedHTML from 'react-sanitized-html';
+
+const HTML_FROM_USER = '<a href="http://bing.com/">Bing</a>';
+
+ReactDOM.render(
+  <SanitizedHTML html={ HTML_FROM_USER } />,
+  document.getElementById('reactRoot')
+);
+```
+
+# Options
+
+You can add [`sanitize-html`](https://npmjs.com/package/sanitize-html) options as props. For example,
+
+```html
+<SanitizedHTML
+  allowedAttributes={['a']}
+  html={ `<a href="http://bing.com/">Bing</a>` }
+/>
+```
+
+You can find more options [here](https://npmjs.com/package/sanitize-html).
+
+# Contribution
+
+Like us? [Star](https://github.com/compulim/react-sanitized-html/stargazers) us.
+
+Found an issue? [File](https://github.com/compulim/react-sanitized-html/issues) us an issue.
---
+++ index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+import React, { PropTypes } from 'react';
+import sanitizeHTML from 'sanitize-html';
+
+const SUPPORTED_SANITIZER_OPTIONS = [
+  'allowProtocolRelative',
+  'allowedAttributes',
+  'allowedClasses',
+  'allowedSchemes',
+  'allowedSchemesByTag',
+  'allowedTags',
+  'exclusiveFilter',
+  'nonTextTags',
+  'parser',
+  'selfClosing',
+  'transformTags'
+];
+
+export default class SanitizedHTML extends React.Component {
+  render() {
+    const sanitizerOptions = SUPPORTED_SANITIZER_OPTIONS.reduce((options, name) => {
+      const value = this.props[name];
+
+      if (typeof value !== 'undefined') {
+        options[name] = value;
+      }
+
+      return options;
+    }, {});
+
+    const sanitizedHTML = sanitizeHTML(
+      this.props.html,
+      sanitizerOptions
+    );
+
+    return (
+      <div
+        className={ this.props.className }
+        dangerouslySetInnerHTML={{ __html: sanitizedHTML }}
+        id={ this.props.id }
+        style={ this.props.style }
+      />
+    );
+  }
+}
+
+SanitizedHTML.defaultProps = {
+  html: ''
+};
+
+SanitizedHTML.propTypes = {
+  allowProtocolRelative: PropTypes.bool,
+  allowedAttributes    : PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
+  allowedClasses       : PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
+  allowedSchemes       : PropTypes.arrayOf(PropTypes.string),
+  allowedSchemesByTag  : PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
+  allowedTags          : PropTypes.arrayOf(PropTypes.string),
+  exclusiveFilter      : PropTypes.func,
+  html                 : PropTypes.string.isRequired,
+  nonTextTags          : PropTypes.arrayOf(PropTypes.string),
+  parser               : PropTypes.any,
+  selfClosing          : PropTypes.arrayOf(PropTypes.string),
+  transformTags        : PropTypes.objectOf(PropTypes.oneOf([PropTypes.func, PropTypes.string])),
+
+  className: PropTypes.string,
+  id       : PropTypes.string,
+  style    : PropTypes.any
+};
